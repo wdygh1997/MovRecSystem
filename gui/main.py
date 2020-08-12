e9e8b782a59bbe8e6868ce0c46d023124e8f362e
萬艼芋 <1214833800@qq.com>
2020-08-12 14:54:04 +0800

    Revert "Update main.py"
    This reverts commit 82b0e68bc78ebda20077fd455c58faf4857cb21b.

--- gui/main.py
+++ gui/main.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 import gui.home
+# import gui.user
+# import gui.search
+# import gui.movie
 
+# 测试home.py页面
 window = tk.Tk()
 window.geometry("800x650")
 window.title("Movie Recommendation System -by wdy")
@@ -8,3 +12,30 @@ frame = tk.Frame(window, width=800, height=650)
 frame.place(x=0, y=0, anchor="nw")
 gui.home.Home(window, frame, None, None)
 window.mainloop()
+
+# 测试user.py页面
+'''window = tk.Tk()
+window.geometry("800x650")
+window.title("Movie Recommendation System -proudly produced by wdy")
+frame = tk.Frame(window, width=800, height=650)
+frame.place(x=0, y=0, anchor="nw")
+gui.user.User(window, frame, 1, 1)
+window.mainloop()'''
+
+# 测试search.py页面
+'''window = tk.Tk()
+window.geometry("800x650")
+window.title("Movie Recommendation System -proudly produced by wdy")
+frame = tk.Frame(window, width=800, height=650)
+frame.place(x=0, y=0, anchor="nw")
+gui.search.Search(window, frame, 1, None)
+window.mainloop()'''
+
+# 测试movie.py页面
+'''window = tk.Tk()
+window.geometry("800x650")
+window.title("Movie Recommendation System -proudly produced by wdy")
+frame = tk.Frame(window, width=800, height=650)
+frame.place(x=0, y=0, anchor="nw")
+gui.movie.Movie(window, frame, 1, 2)
+window.mainloop()'''
